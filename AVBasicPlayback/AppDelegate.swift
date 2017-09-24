358ee705d103a087d1f8a2de0e3ee9307915922a
Masanori Kuze <masanori@masanori-no-imac.local>
2017-09-25 00:51:43 +0900

    first commit

--- AVBasicPlayback/AppDelegate.swift
+++ AVBasicPlayback/AppDelegate.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
+
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    @IBOutlet weak var playerView: PlayerView!
+    var player : AVPlayer!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        
+//        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
         return true
     }
 
@@ -27,6 +61,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        playerView.player = nil
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,12 +71,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+        if(playerView != nil){
+            playerView.player = player
+        }
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    override func remoteControlReceived(with event: UIEvent?) {
+        let rc = event!.subtype
+        print("does this work? \(rc.rawValue)")
+    }
 }
 
--- AVBasicPlayback/ViewController.swift
+++ AVBasicPlayback/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    var movieId : URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let albumQuery = MPMediaQuery.songs()
+        let albums = albumQuery.collections as [MPMediaItemCollection]!
+        
+        guard (albumQuery.collections as [MPMediaItemCollection]!) != nil else {
+            return
+        }
+        
+        for album in albums! {
+            let rv = album.representativeItem?.mediaType.rawValue
+            if(rv == 2049 || rv == 2048){
+                movieId = album.representativeItem?.assetURL
+            }
+//            print(album.representativeItem?.title ?? "")
+//            print(album.representativeItem?.mediaType ?? "")
+        }
+        
+        UIApplication.shared.beginReceivingRemoteControlEvents()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +44,53 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func playVideo(_ sender: Any) {
+    
+//        guard let url = URL(string: "https://devimages.apple.com.edgekey.net/samplecode/avfoundationMedia/AVFoundationQueuePlayer_HLS2/master.m3u8") else {
+//            return
+//        }
+        
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+        
+        do {
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("fatalError")
+            fatalError()
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: movieId)
+        let playerItem = player.currentItem
+
+        let tracks = playerItem?.tracks
+        for playerItemTrack in tracks! {
+            if playerItemTrack.assetTrack.hasMediaCharacteristic(AVMediaCharacteristic.visual) {
+                playerItemTrack.isEnabled = false
+            }
+        }
+        
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let playerView = PlayerView()
+        
+        
+        self.view = playerView
+        (self.view.layer as! AVPlayerLayer).player = player
+        
+        appDelegate.player = player
+        appDelegate.playerView = playerView
+
+        player.play()
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+//        let controller = AVPlayerViewController()
+//        controller.player = player
+//
+//        // Modally present the player and call the player's play() method when complete.
+//        present(controller, animated: true) {
+//            player.play()
+//        }
+    }
 }
 
